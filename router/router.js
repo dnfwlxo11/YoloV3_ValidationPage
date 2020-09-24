87436bf7c4a7ee6c0c3e37da195473484060fc8d
dnfwlxo11 <dnfwlxo11@naver.com>
2020-09-24 12:47:45 +0900

    add async func (no active)

--- router/router.js
+++ router/router.js
@@ -18,25 +18,40 @@ var upload = multer({
     }),
 });
 
-router.post('/upload_file', upload.single('img'), function (req, res) {
-    console.log(req.file);
+async function startLabeling(name){
+    var fn = name;
+    var filePath;
+    console.log('\n' + fn)
+    var date = new Date();
+    //date = date.toLocaleDateString().replace('. ', '_') + '_'.toString() + date.getHours.toString() + '_'.toString() + date.getMinutes.toString() + '_'.toString() + date.getMilliseconds.toString();
+    //console.log(date);
+    
+    await execFile('./startLabeling', [fn, fn.substring(0, fn.lastIndexOf('.')) + '.txt', fn.substring(0, fn.lastIndexOf('.')) + '.txt',
+        'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg', 'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg',
+        fn, fn.substring(0, fn.lastIndexOf('.')) + '.jpg'], function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(data.toString());
+                filePath = '/../predictions.jpg';
+            }
+        }
+    );
+
+    return filePath
+}
+
+router.post('/upload_file', upload.array('img'), function (req, res) {
+    console.log(req.files[2]);
     console.log('success save file');
 
-    var fn = req.file.originalname
-    var date = new Date();
-    date = date.toLocaleDateString().replace('. ', '_') + '_'.toString() + date.getHours.toString() + '_'.toString() + date.getMinutes.toString() + '_'.toString() + date.getMilliseconds.toString();
-    console.log(date)
-    execFile('./startLabeling', [fn, fn.substring(0, fn.lastIndexOf('.')) + 'txt', fn.substring(0, fn.lastIndexOf('.')) + '.txt', 
-            'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg', 'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg', 
-            fn, fn.substring(0, fn.lastIndexOf('.')), 'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg'], function (err, data) {
-        if (err) {
-            console.log(err)
-        }
-        else {
-            console.log(data.toString());
-            res.sendFile(path.join(__dirname, '/../predictions.jpg'));
-        }
-    });
+    for (var i = 0; i < req.files.length; i++) {
+        var name = req.files[i].originalname;
+        var tmp = startLabeling(name);
+        console.log(tmp)
+        //res.sendFile(path.join(__dirname));
+    }
 });
 
 router.get('/', function (req, res) {
