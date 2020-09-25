0a129fca850fcc5c03f4ead62440b7d6681777d0
dnfwlxo11 <dnfwlxo11@naver.com>
2020-09-25 13:26:47 +0900

    edit async code (no active)

--- router/router.js
+++ router/router.js
@@ -18,56 +18,44 @@ var upload = multer({
     }),
 });
 
-async function startLabeling(name){
-    var fn = name;
-    var filePath;
-    console.log('\n' + fn)
-    var date = new Date();
-    //date = date.toLocaleDateString().replace('. ', '_') + '_'.toString() + date.getHours.toString() + '_'.toString() + date.getMinutes.toString() + '_'.toString() + date.getMilliseconds.toString();
-    //console.log(date);
-    
-    await execFile('./startLabeling', [fn, fn.substring(0, fn.lastIndexOf('.')) + '.txt', fn.substring(0, fn.lastIndexOf('.')) + '.txt',
-        'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg', 'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg',
-        fn, fn.substring(0, fn.lastIndexOf('.')) + '.jpg'], function (err, data) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log(data.toString());
-                filePath = '/../predictions.jpg';
-            }
-        }
-    );
 
-    return filePath
-}
 
 router.post('/upload_file', upload.array('img'), function (req, res) {
-    console.log(req.files[2]);
     console.log('success save file');
+    var imagegItem = req.files;
 
-    for (var i = 0; i < req.files.length; i++) {
-        var name = req.files[i].originalname;
-        var tmp = startLabeling(name);
-        console.log(tmp)
-        //res.sendFile(path.join(__dirname));
+    async function startLabeling(name){
+            var fn = name;
+            
+            execFile('./startLabeling', [fn, fn.substring(0, fn.lastIndexOf('.')) + '.txt', fn.substring(0, fn.lastIndexOf('.')) + '.txt',
+                'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg', 'predict_' + fn.substring(0, fn.lastIndexOf('.')) + '.jpg',
+                fn, fn.substring(0, fn.lastIndexOf('.')) + '.jpg'], function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    }
+
+                    else {
+                        console.log(data.toString());
+                        res.sendFile(path.join(__dirname, '../predictions.jpg'));
+                    }
+                }
+            );
     }
+
+    async function asyncLaunch(imagegItem) {
+        imagegItem.forEach(item => {
+            var name = item.originalname;
+            await startLabeling(name);
+            console.log(item);
+        })
+    }
+
+    asyncLaunch();
 });
 
 router.get('/', function (req, res) {
     console.log("Hi welcome my server");
     res.sendFile(path.join(__dirname, '../public/main.html'));
 });
-
-// router.post('/email_post', function (req, res) {
-//     res.render(path.join(__dirname, '../public/email.ejs'), { 'email': req.body.email });
-//     // res.sendFile(__dirname + '/public/main.html');
-// });
-
-// router.post('/ajax_home', function (req, res) {
-//     console.log('data loading complate : ' + req.body.new);
-//     var responseData = { 'result': 'ok', 'receive data': req.body.new };
-//     res.json(responseData)
-// });
 
 module.exports = router;
